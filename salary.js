6da4d2e83061a11fdf0f69fa64f503cde0d23476
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2022-12-06 19:39:55 +0300

    changed salary.js and speed.js

--- salary.js
+++ salary.js
@@ -1,91 +1,125 @@
+// declaring our variable and initializing it to integer 0
+let basicSal=0;
+let totalBenefits=0;
+let nssf=200;
 
-/*
-   This program outputs  to the user their tax deductions, nhif, nssf, the total amount deducted then prints out the  
-    Gross salary = all_benefits + basic salary e.g house_allowance + hardship_allowance + risk_allowance + basic_salary
-    Taxable income = Gross pay
-    Deductions = (Tax + NHIF + NSSF)
-    Earnable pay = Gross pay - deductions e.g Gross salary - (Tax + NHIF + NSSF) ==> net salary
-    Write a program whose major task is to calculate an individual’s Net Salary by getting the inputs of basic salary and benefits. Calculate the payee (i.e. Tax), NHIF Deductions, NSSF Deductions, gross salary, and net salary. 
+// start of our function
+function netSalary(){
+    let basicSal = prompt("Please enter your Basic Salary: "); //user prompt
+    document.getElementById("basic").innerHTML = basicSal; //Output
+// If basic salary is not null
+    if (basicSal!=null){
+        let totalBenefits = prompt("Please enter your total Benefits`: "); //user prompt
+        document.getElementById("benefits").innerHTML = totalBenefits; //Output
+        let grossSalary = parseInt(basicSal) + parseInt(totalBenefits);
+        document.getElementById("gross").innerHTML = grossSalary; //Output
+        
+        //we calculate the nhif deductions according to ones earnings
+        function NHIF(){
+            if(grossSalary<=5999){
+                let nhif=150;
+                document.getElementById("nhif").innerHTML = nhif;
+            }
+            else if(grossSalary>=6000 && grossSalary<=7999){
+                let nhif=300;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=8000 && grossSalary<=11999){
+                let nhif=400;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=12000 && grossSalary<=14999){
+                let nhif=500;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=15000 && grossSalary<=19999){
+                let nhif=600;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=20000 && grossSalary<=24999){
+                let nhif=750;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=25000 && grossSalary<=29999){
+                let nhif=850;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=30000 && grossSalary<=34999){
+                let nhif=900;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=35000 && grossSalary<=39999){
+                let nhif=950;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=40000 && grossSalary<=44999){
+                let nhif=1000;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=45000 && grossSalary<=49999){
+                let nhif=1100;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=50000 && grossSalary<=59999){
+                let nhif=1200;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=60000 && grossSalary<=69999){
+                let nhif=1300;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=70000 && grossSalary<=79999){
+                let nhif=1400;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=80000 && grossSalary<=89999){
+                let nhif=1500;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else if(grossSalary>=90000 && grossSalary<=99999){
+                let nhif=1600;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+            else{
+                let nhif=1700;
+                document.getElementById("nhif").innerHTML = nhif; 
+            }
+        }
+        NHIF();S
+        //this section calculates all payee salaries
+        function PAYEE(){
+            grossSalary=parseInt(grossSalary);
+            if(grossSalary<=24000){
+                let tax = 0.1 * grossSalary;
+                document.getElementById("payee").innerHTML = tax; 
+            }
+            else if(grossSalary>=24001 && grossSalary<=32333){
+                let tax = 0.25 * grossSalary;
+                document.getElementById("payee").innerHTML = tax; 
+            }
+            else if(grossSalary>32333){
+                let tax = 0.3 * grossSalary;
+                document.getElementById("payee").innerHTML = tax; 
+            }
+        }
+        PAYEE();
 
-NB: Use KRA, NHIF, and NSSF values provided//
-*/
+        //getting our values
+        let totalDeductions=parseInt(document.getElementById("nhif").innerHTML) +
+        parseInt(document.getElementById("payee").innerHTML) +
+        parseInt(document.getElementById("nssf").innerHTML); //expect outcome
+        
+ document.getElementById("deductions").innerHTML=totalDeductions;
 
-const readline = require('readline');
-const input = readline.createInterface(
-    {
-        input: process.stdin,
-        output: process.stdout
+        let netSal= parseInt(document.getElementById("gross").innerHTML) -
+        parseInt(document.getElementById("deductions").innerHTML);
+        document.getElementById("net").innerHTML=netSal;
     }
-);
-
-input.question("ENTER YOUR GROSS SALARY?\n N/B: Gross salary=Basic salary+Deductions\n", function (gross_pay){
-    
-    let Tax;
-    let extra_taxable_pay;
-    let nhif;
-    let nssf= 200;
-    let total_deductions;
-    let net_salary;
-
-    //calcluating the taxes which are required to tabulate the deductions//
-    if (gross_pay < 24001){
-        Tax = gross_pay * 0.1;
-    }else if (gross_pay < 32334){
-        Tax = 2400;
-        extra_taxable_pay = gross_pay - 24000;
-        Tax = (extra_taxable_pay * 0.25) + Tax;
-    }else {
-        Tax = 4483;
-        extra_taxable_pay = gross_pay - 32333;
-        Tax = (extra_taxable_pay * 0.30) + Tax;   
-       //calculating NHIFs for the repective gross_pay//
+// If Basic Salary is null
+    else{
+        document.getElementById("basic").innerHTML = basic; 
+        document.getElementById("benefits").innerHTML = totalBenefits; 
+        let grossSalary = basicSal + totalBenefits;
+        document.getElementById("gross").innerHTML = grossSalary; 
     }
-    if (gross_pay < 6000){
-        nhif = 150;
-    }else if(gross_pay < 8000){
-        nhif = 300;
-    }else if(gross_pay< 12000){
-        nhif = 400;
-    }else if(gross_pay < 15000){
-        nhif = 500;
-    }else if(gross_pay < 20000){
-        nhif = 600;
-    }else if(gross_pay < 25000){
-        nhif = 750;
-    }else if(gross_pay < 30000){
-        nhif = 850;
-    }else if(gross_pay < 35000){
-        nhif = 900;
-    }else if(gross_pay < 40000){
-        nhif = 950;
-    }else if(gross_pay < 45000){
-        nhif = 1000;
-    }else if(gross_pay < 50000){
-        nhif = 1100;
-    }else if(gross_pay < 60000){
-        nhif = 1200;
-    }else if(gross_pay < 70000){
-        nhif = 1300;
-    }else if(gross_pay < 80000){
-        nhif = 1400;
-    }else if(gross_pay < 90000){
-        nhif = 1500;
-    }else if(gross_pay > 100000){
-         nhif = 1600;
-    }else{
-        nhif = 1700;
-    }
-// this are the necessary arithmetics required for the net salary calculation.
-//the process.stdout.write are used to print out the resulting tabulations after the deductions incurred
-    total_deductions = nhif + Tax + nssf;
-    net_salary = gross_pay - total_deductions;
-    process.stdout.write(`Your gross pay is Ksh: ${gross_pay}\n`);
-    process.stdout.write(`Your P.A.Y.E is Ksh: ${Tax}\n`);
-    process.stdout.write(`Your NHIF deduction pay is Ksh: ${nhif}\n`);
-    process.stdout.write(`Your standard NSSF deduction is Ksh: ${nssf}\n`);    
-    process.stdout.write(`Your Total deduction amount to Ksh: ${total_deductions}\n`);
-    process.stdout.write(`Your Net Salary is Ksh: ${net_salary}\n`);    
-
-    input.close();
- }
-);
+}
--- speed.js
+++ speed.js
@@ -1,41 +1,19 @@
-    
-/*Speed is input an integer
-the programs uses the  readline.createInterface()  to input user values and give results
-for Speed limit < 70km/s;  the program is set to print OK! meaning we are safe haha! 
- but for Speed limits >70km/s, the program is set to demerit the driver 1 point for every 5 km/s speed above the set speed limit print total number of demerit point.
- if the demerit points are above 12 the the drivers licence is to be suspended,.# say no to reckless driving
+function promptSpeed(){
+    speed = prompt("Please Enter your speed");
 
- question:Write a program that takes the speed of a car as input e.g 80. If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), 
- it should give the driver one demerit point and print the total number of demerit points.
-*/
-
-const readline = require('readline');
-const input = readline.createInterface(
-{
-    input: process.stdin,
-    output: process.stdout
+let points = (speed-70)/5;
+//if speed is <= 70 the driver gets a ok compliment
+    if(speed <=70){
+        console.log("OK");
+    }
+    //if the demerit points exceed 12 the driver is considered to be overspeeding hence their lincense gets suspended
+    else if (points > 12){
+        console.log("License suspended");
+    }
+    //else give the demerit points
+    else{
+        console.log(`points: ${points}`);
+    }
 }
-);
-
-input.question("Speed of the car: ", function (speed){ 
-let speed_of_car = parseInt(speed, 10);
-if (isNaN(speed)){
-    process.stdout.write("Speed must be a numerical value\n");
-    process.stdout.write("use this a follows: On prompt input as numerical value e.g 50\n");
-    process.exit(1)
-}else{
-   if (speed_of_car < 70){
-    process.stdout.write("Ok!\n");
-   } else{
-    const demerit_points = ((speed_of_car - 70) / 5);
-    Math.ceil(demerit_points);
-    process.stdout.write(`Points: ${demerit_points}\n`);
-        if (demerit_points >= 12) {
-            process.stdout.write("License suspended.\n");                
-        } else {
-            process.exit(0);                
-        }
-   }
-}
-input.close();
-});
+//calling the back
+promptSpeed();
